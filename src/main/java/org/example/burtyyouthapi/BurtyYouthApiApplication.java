938fa9f519148af5cb08307f72b37784b916658f
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-04-21 15:11:00 +0900

    first commit

---
+++ src/main/java/org/example/burtyyouthapi/BurtyYouthApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.burtyyouthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurtyYouthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurtyYouthApiApplication.class, args);
+    }
+
+}
