993c6cb10598b53868011b11d0606b8097556369
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-04-22 17:40:15 +0900

    feat: swagger 설정 추가

--- src/main/java/org/example/burtyyouthapi/BurtyYouthApiApplication.java
+++ src/main/java/org/example/burtyyouthapi/BurtyYouthApiApplication.java
@@ -2,7 +2,17 @@ package org.example.burtyyouthapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+@OpenAPIDefinition(
+        info = @Info(
+                title       = "Burty Youth Policy API",
+                version     = "v0.0.1",
+                description = "청년정책 상세 검색 및 조회 API",
+                contact     = @Contact(name = "팀 Burty", email = "team@burty.example.com")
+        )
+)
 @SpringBootApplication
 public class BurtyYouthApiApplication {
 
