c704fe7592c8cfb62b27c6953288518647f8db73
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-04-23 11:37:12 +0900

    feat: 콘텐츠 목록 반환용 dto

---
+++ src/main/java/org/example/burtyyouthapi/content/dto/ContentDto.java
@@ -0,0 +1,28 @@
+package org.example.burtyyouthapi.content.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 청년 콘텐츠 목록 반환용 DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContentDto {
+    /** 게시물 일련번호 */
+    private String pstSn;
+    /** 게시물 구분명 */
+    private String pstSeNm;
+    /** 게시물 제목 */
+    private String pstTtl;
+    /** 첨부 파일 (Base64) */
+    private String atchFile;
+    /** 게시물 전체 내용 */
+    private String pstWholCn;
+    /** 최초 등록 일시 */
+    private String frstRegDt;
+    /** 최종 수정자명 */
+    private String lastMdfrNm;
+}
